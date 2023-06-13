20a354cf4f9c72dd55615912d9018765cf0a07e5
AndriiZnam <68700223+AndriiZnam@users.noreply.github.com>
2023-06-13 17:03:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ bot = telebot.TeleBot("5878101695:AAEW5OX8ZHqazaOy6xi0y-52X1kbDbFShsg")
 def repeat_all_messages(message): # Название функции не играет никакой роли
     bot.send_message(message.chat.id, message.text)
 
-#if __name__ == '__main__':
- #    bot.infinity_polling()
+if __name__ == '__main__':
+     bot.polling()
