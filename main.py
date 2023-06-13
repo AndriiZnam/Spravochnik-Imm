14a3e01cc172d100301a1a748e1916cc512839d2
AndriiZnam <68700223+AndriiZnam@users.noreply.github.com>
2023-06-13 16:56:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-try:
-  print("Hello world")
-except Exception as e:
-  logging.error("An error occurred: %s", type(e).__name__)
-  logging.exception("Error details:") 
+
+import telebot
+
+bot = telebot.TeleBot("5878101695:AAEW5OX8ZHqazaOy6xi0y-52X1kbDbFShsg")
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
